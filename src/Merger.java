2c25355a2bf138e5ce42e745b09abc82727ef536
jondef95@vt.edu <jondef95@github.com>
2016-03-30 00:29:14 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Buffer.java changed at Wed Mar 30 00:29:13 EDT 2016

--- src/Merger.java
+++ src/Merger.java
@@ -1,83 +1,43 @@
 
 /**
- * The actual mergesort implementation for the program
- * USING INTS FOR THE TIME BEING/ DON'T KNOW WHAT IT WILL NEED TO BE
+ * The actual mergesort implementation for the program USING INTS FOR THE TIME
+ * BEING/ DON'T KNOW WHAT IT WILL NEED TO BE
+ * 
  * @author platt, jondef95
  * @version 1
  *
  */
-public class Merger {
+public class Merger
+{
 
-    private int[] inArray;
-    private int[] tempArray;
-    private int length;
-    /**
-     * starts the mergesort class, don't really know what this will need
-     */
-    public Merger() {
+    public Merger()
+    {
         // TODO Auto-generated constructor stub
     }
-    
-    /**
-     * the original input of the array
-     * @param input the input array
-     */
-    public void sort(int[] input)
-    {
-        inArray = input;
-        length = input.length;
-        tempArray = new int[length];
-        mergeSort(0, length - 1);
-    }
-    
-    /**
-     * does the splitting of the values
-     * @param bottom is the lower index
-     * @param top is the higher index
-     */
-    private void mergeSort(int bottom, int top)
-    {
-        if (bottom < top)
-        {
-            int middle = (bottom + ((top - bottom)/2));
-            //sort left hand side (lower side)
-            mergeSort(bottom, middle);
-            //sort right hand side (higher side)
-            mergeSort(middle + 1, top);
-            //re-assemble the parts again
-            assemble(bottom, middle, top);
-        }
-    }
-    
-    private void assemble(int bottom, int middle, int top)
-    {
-        for (int i = bottom; i <= top; i++)
-        {
-            tempArray[i] = inArray[i];
-        }
-        int i = bottom;
-        int j = middle + 1;
-        int k = top;
-        while(i <= middle && j <= top)
-        {
-            if (tempArray[i] <= tempArray[j])
-            {
-                inArray[k] = tempArray[i];
-                i++;
-            }
-            else
-            {
-                inArray[k] = tempArray[j];
-                j++;
-            }
-            k++;
-        }
-        while(i <= middle)
-        {
-            inArray[k] = tempArray[i];
-            k++;
-            i++;
-        }
-    }
 
+//    public void mergesort(byte[] A, Comparable[] temp, int left,
+//            int right)
+//    {
+//        if (left == right)
+//            return; // List has one record
+//        int mid = (left + right) / 2; // Select midpoint
+//        mergesort(A, temp, left, mid); // Mergesort first half
+//        mergesort(A, temp, mid + 1, right); // Mergesort second half
+//        for (int i = left; i <= right; i++) // Copy subarray to temp
+//            temp[i] = A[i];
+//        // Do the merge operation back to A
+//        int i1 = left;
+//        int i2 = mid + 1;
+//        for (int curr = left; curr <= right; curr++)
+//        {
+//            if (i1 == mid + 1) // Left sublist exhausted
+//                A[curr] = temp[i2++];
+//            else if (i2 > right) // Right sublist exhausted
+//                A[curr] = temp[i1++];
+//            else if (temp[i1].compareTo(temp[i2]) <= 0) // Get smaller value
+//                A[curr] = temp[i1++];
+//            else
+//                A[curr] = temp[i2++];
+//        }
+//    }
 }
