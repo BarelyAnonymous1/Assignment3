9258d629d06e00cb8a56821f6232882a13eb997c
Jonathan <jondef95@vt.edu>
2016-03-31 15:07:02 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Thu Mar 31 15:07:01 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -1,31 +1,42 @@
 import java.io.*;
+
 /**
  * The class containing the main method, the entry point of the application.
  * 
  * @author { your name here }
  * @version { put something here }
  */
-public class Mergesort {
+public class Mergesort
+{
 
-	/**
-	 * The entry point of the application
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-	    RandomAccessFile input;
-	    try{
-	        input = new RandomAccessFile(args[0], "rw");
-	    }
-	    catch (FileNotFoundException e)
-	    {
-	        System.out.println("File '" + args[0] + "' was not found");
-	    }
-	    
-		BufferPool bufpool = new BufferPool(3);
-		bufpool.newBuffer(1, null);
-		bufpool.newBuffer(2, null);
-		bufpool.newBuffer(3, null);
-		bufpool.newBuffer(4, null);
-	}
+    /**
+     * The entry point of the application
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        RandomAccessFile input;
+        try
+        {
+            input = new RandomAccessFile(args[0], "rw");
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File '" + args[0] + "' was not found");
+        }
+        int numBuffers = Integer.parseInt(args[1]);
+        if (numBuffers < 1)
+        {
+            System.out.println("Please provide at least 1 buffer for the buffer pool");
+            return;
+        }
+        
+        BufferPool sortPool = new BufferPool(numBuffers);
+        BufferPool bufpool = new BufferPool(3);
+        bufpool.newBuffer(1, null);
+        bufpool.newBuffer(2, null);
+        bufpool.newBuffer(3, null);
+        bufpool.newBuffer(4, null);
+    }
 }
