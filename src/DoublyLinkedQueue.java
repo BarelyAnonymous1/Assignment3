e50563271ce8ca45b50cc30cf9cb216420fbada7
jondef95@vt.edu <jondef95@github.com>
2016-03-30 23:23:57 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/DoublyLinkedQueue.java changed at Wed Mar 30 23:23:56 EDT 2016

--- src/DoublyLinkedQueue.java
+++ src/DoublyLinkedQueue.java
@@ -38,7 +38,7 @@ public class DoublyLinkedQueue
     public void enqueue(Buffer buffer)
     {
         DoublyLinkedNode newNode = new DoublyLinkedNode(buffer);
-        tail.getPrev().setNext(newNode);
+        tail.getPrev();
         newNode.setPrev(tail.getPrev());
         tail.setPrev(newNode);
         newNode.setNext(tail);
