ad0abc4d07f98b6180662e54f2c79b4c551972d0
jondef95@vt.edu <jondef95@github.com>
2016-03-30 22:52:44 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/LRUQueue.java changed at Wed Mar 30 22:52:44 EDT 2016

--- src/LRUQueue.java
+++ src/LRUQueue.java
@@ -7,6 +7,32 @@ public class LRUQueue
     public LRUQueue(int max)
     {
         MAX_SIZE = max;
-
+        list = new DoublyLinkedQueue();
     }
+    
+    /**
+     * will add a buffer to the list if the ID and file name dont already exist in a buffer in the list. if the list was shifted or 
+     * @param newBuffer
+     * @return
+     */
+    public Buffer addOrShift(Buffer newBuffer)
+    {
+        Buffer existingBuffer = list.remove(newBuffer.getID(), newBuffer.getFile());
+        if (existingBuffer == null)
+        {
+            list.enqueue(newBuffer);
+            if (list.getSize() > MAX_SIZE)
+                return list.dequeue();
+            else
+                return null;
+        }
+        else
+        {
+            list.enqueue(existingBuffer);
+            return null;
+        }
+    }
+    
+    public byte[] removeLRU()
+    
 }
