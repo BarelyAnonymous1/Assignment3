b86f76f235416b187aff38d6c09428cc9568fb37
jondef95@vt.edu <jondef95@github.com>
2016-03-30 23:54:38 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferPool.java changed at Wed Mar 30 23:54:37 EDT 2016

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,4 +1,4 @@
-
+import java.io.*;
 public class BufferPool
 {
     private LRUQueue pool;
@@ -11,8 +11,27 @@ public class BufferPool
         pool = new LRUQueue(startMax);
         for (int i = 0; i < maxBuffers; i++)
         {
+            // the ID for each filler Buffer is so the Buffer pool knows to do nothing 
+            // with it when it is removed
             pool.addOrShift(new Buffer(null, (-1)*(i+1), null));
         }
+    }
+    
+    public Buffer newBuffer(int searchID, File searchFile)
+    {
+        Buffer returnBuffer = getBuffer(searchID, searchFile);
+    }
+    
+    public Buffer getBuffer(int searchID, File searchFile)
+    {
+        DoublyLinkedNode existNode = pool.getLRUQueue().remove(searchID, searchFile);
+        if (existNode != null)
+        {
+            pool.getLRUQueue().enqueue(existNode);
+            return existNode.getData();
+        }
+        else
+            return null;
     }
 
 }
