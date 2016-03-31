7fee1ee3f174d788fb51a4e29b8efabe64472410
Preston Lattimer <preston.lattimer@gmail.com>
2016-03-31 09:48:01 -0400

    test

--- src/Buffer.java
+++ src/Buffer.java
@@ -7,6 +7,7 @@ public class Buffer
     private byte[] block;
     private int    index;
     private int blockSize;
+    private RandomAccessFile file;
 
     public Buffer(byte[] newPage, int pageIndex, int newBlockSize)
     {
@@ -14,24 +15,34 @@ public class Buffer
         index = pageIndex;
         blockSize = newBlockSize;
     }
-
+    /**
+     * which block in the file this is (1, 2, 3... multiples of 4096)
+     * @return the index
+     */
     public int getIndex()
     {
         return index;
     }
 
-    public byte[] getBlock()
-    {
-        return block;
-    }
-
-    public void setBlock(byte[] newPage)
-    {
-        block = newPage;
-    }
+    
+//    public byte[] getBlock()
+//    {
+//        return block;
+//    }
+//
+//    public void setBlock(byte[] newPage)
+//    {
+//        block = newPage;
+//    }
 
     public byte[] getRecord(int pos)
     {
         return Arrays.copyOfRange(block, pos % blockSize, 4);
+    }
+    
+   
+    public void flush()
+    {
+        
     }
 }
