c61bd0fe5ec0ac2f03168c8bf67674d5d947fcb7
Jonathan <jondef95@vt.edu>
2016-03-31 14:58:55 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Thu Mar 31 14:58:55 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -13,7 +13,14 @@ public class Mergesort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    
+	    RandomAccessFile input;
+	    try{
+	        input = new RandomAccessFile(args[0], "rw");
+	    }
+	    catch (FileNotFoundException e)
+	    {
+	        System.out.println("File '" + args[0] + "' was not found");
+	    }
 	    
 		BufferPool bufpool = new BufferPool(3);
 		bufpool.newBuffer(1, null);
