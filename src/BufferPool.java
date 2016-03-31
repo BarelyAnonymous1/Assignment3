4963740b94d1e4cb1b4ddcb2d0b907c689d6a80d
jondef95@vt.edu <jondef95@github.com>
2016-03-30 23:13:13 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferPool.java changed at Wed Mar 30 23:13:12 EDT 2016

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -2,5 +2,15 @@
 public class BufferPool
 {
     private LRUQueue pool;
+    private int BUFFER_SIZE = 4096;
+    private int maxBuffers;
+    
+    public BufferPool(int startMax)
+    {
+        maxBuffers = startMax;
+        pool = new LRUQueue(startMax);
+        for (int i = 0; i < maxBuffers; i++)
+            pool.addOrShift(new Buffer(null, -1, null));
+    }
 
 }
