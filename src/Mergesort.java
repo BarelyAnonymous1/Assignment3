a8d7b9526a3e415f4531ccdb89183cc51e3ece27
jondef95@vt.edu <jondef95@github.com>
2016-03-31 22:58:30 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Thu Mar 31 22:58:29 EDT 2016
    trying to get buffers working

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -52,7 +52,7 @@ public class Mergesort
             bufpool.newBuffer(1, null);
             bufpool.newBuffer(0, input);
             bufpool.getBuffer(0, input).storeBlock();
-            System.out.println(bufpool.getBuffer(0, input).getRecord(0));
+            System.out.println(bufpool.getBuffer(0, input).getRecord(0).toString());
         }
         catch(IOException e)
         {
