3c280946ad39e66a48c393a7d89b6088514839e7
Jonathan <jondef95@vt.edu>
2016-03-31 21:54:20 -0400

    started the file IO

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -19,32 +19,30 @@ public class Mergesort
         RandomAccessFile input;
         try
         {
-            input = new RandomAccessFile("garbage.txt", "rw");
+            input = new RandomAccessFile(args[0], "rw");
         }
         catch (IOException e)
         {
-            System.out.println("File '" + "garbage.txt" + "' was not found");
+            System.out.println("File '" + args[0] + "' was not found");
         }
         
-        //int numBuffers = Integer.parseInt(args[1]);
-        
+        int numBuffers = Integer.parseInt(args[1]);
         RandomAccessFile stats;
         try
         {
-            //stats = new RandomAccessFile(args[2], "w");
+            stats = new RandomAccessFile(args[2], "rw");
         }
         catch(IOException e)
         {
             System.out.println("There was an error");
         }
         
-        if (5 < 1)
+        if (numBuffers < 1)
         {
             System.out.println(
                     "Please provide at least 1 buffer for the buffer pool");
             return;
         }
-        
 
         BufferPool sortPool = new BufferPool(numBuffers);
         BufferPool bufpool = new BufferPool(3);
