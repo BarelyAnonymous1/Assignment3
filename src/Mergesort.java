9822e5084d2e26a986abe37823f1194c42e7caeb
Jonathan <jondef95@vt.edu>
2016-04-04 13:53:27 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Mon Apr 04 13:53:27 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -54,7 +54,7 @@ public class Mergesort
             bufpool.newBuffer(4096, input);
             bufpool.newBuffer(0,  temp);
             bufpool.getBuffer(1, input).storeBlock();
-            bufpool.getBuffer(0, temp).setBlock(bufpool.getBuffer(1, input).getBlock());
+            bufpool.getBuffer(0, temp).setBlock(bufpool.getBuffer(1, input).getRealBlock());
             byte[] output = bufpool.getRecord(temp, 8187);
             System.out.println(output[0]);
             System.out.println(output[1]);
