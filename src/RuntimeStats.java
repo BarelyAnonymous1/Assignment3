c009cd2cbe141d6f79e0b755bf6330169204bc09
Jonathan <jondef95@vt.edu>
2016-04-05 14:09:14 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/RuntimeStats.java changed at Tue Apr 05 14:09:13 EDT 2016

--- src/RuntimeStats.java
+++ src/RuntimeStats.java
@@ -2,15 +2,17 @@
 public class RuntimeStats
 {
     public static String file;
+    public static int numBuffers;
     public static int foundInBuffer;
     public static int readDisk;
     public static int writeDisk;
     public static int startTime;
     public static int endTime;
     
-    public RuntimeStats(String filename)
+    public RuntimeStats(String filename, int numberOfBuffers)
     {
         file = filename;
+        numBuffers = numberOfBuffers;
         foundInBuffer = 0;
         readDisk = 0;
         writeDisk = 0;
