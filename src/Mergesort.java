0b6b157993875426a87ffd55a9b411baa3e18acc
jondef95@vt.edu <jondef95@github.com>
2016-04-04 22:10:43 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Mon Apr 04 22:10:42 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -53,6 +53,7 @@ public class Mergesort
             BufferPool bufpool = new BufferPool(5);
             bufpool.newBuffer(0,  temp);
             byte[] output = bufpool.getRecord(8188, input);
+            bufpool.writeRecord(8188, output, temp);
             System.out.println(output[0]);
             System.out.println(output[1]);
             System.out.println(output[2]);
