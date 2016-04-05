4d99ef1b21d713818f56436e3eb216049194fa2f
Jonathan <jondef95@vt.edu>
2016-04-05 14:05:48 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/RuntimeStats.java changed at Tue Apr 05 14:05:48 EDT 2016

--- src/RuntimeStats.java
+++ src/RuntimeStats.java
@@ -1,5 +1,17 @@
 
 public class RuntimeStats
 {
+    public static String file;
+    public static int foundInBuffer;
+    public static int readDisk;
+    public static int writeDisk;
+    public static int startTime;
+    public static int endTime;
     
+    
+    
+    public String toString()
+    {
+        return null;
+    }
 }
