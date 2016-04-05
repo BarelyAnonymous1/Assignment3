6a7dfa17f7afafa14fd29c3ba244d4bb296caa35
jondef95@vt.edu <jondef95@github.com>
2016-04-04 23:10:51 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/LRUQueue.java changed at Mon Apr 04 23:10:51 EDT 2016

--- src/LRUQueue.java
+++ src/LRUQueue.java
@@ -35,7 +35,7 @@ public class LRUQueue
     
     public Buffer removeLRU()
     {
-        return list.dequeue().getData();        
+        return list.dequeue().getData(); 
     }
     
     public int getSize()
