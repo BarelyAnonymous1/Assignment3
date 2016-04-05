badc8bc6472d12d0f888a6047b6a35b8c4dfad52
jondef95@vt.edu <jondef95@github.com>
2016-04-04 21:43:22 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Mon Apr 04 21:43:21 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -56,7 +56,7 @@ public class Mergesort
             System.out.println(output[0]);
             System.out.println(output[1]);
             System.out.println(output[2]);
-            System.out.println(output[3]);
+            System.out.println(output[4]);
         }
         catch (IOException e)
         {
