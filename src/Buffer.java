e30a180e02d802014ef7c76bb059a7d23baebc78
Jonathan <jondef95@vt.edu>
2016-04-05 14:42:10 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Buffer.java changed at Tue Apr 05 14:42:09 EDT 2016

--- src/Buffer.java
+++ src/Buffer.java
@@ -22,6 +22,7 @@ public class Buffer
     public Buffer(int startPosition, RandomAccessFile startFile)
     {
         block = new byte[BufferPool.BUFFER_SIZE];
+        RuntimeStats.newCalls++;
         index = startPosition / BufferPool.BUFFER_SIZE;
         file = startFile;
         hasBlock = false;
