bdf08f3f9a76d01c0b405078115080b0a2c9fa13
Jonathan <jondef95@vt.edu>
2016-04-06 12:47:15 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Buffer.java changed at Wed Apr 06 12:46:07 EDT 2016

--- src/Buffer.java
+++ src/Buffer.java
@@ -6,6 +6,7 @@ public class Buffer
     private byte[]           block;
     private int              index;
     private boolean          dirtyBit;
+    private int size;
 
     private RandomAccessFile file;
 
@@ -47,7 +48,14 @@ public class Buffer
             if (file != null)
             {
                 file.seek(index * BufferPool.BUFFER_SIZE);
-                file.read(block);
+                if ((index + 1)
+                        * BufferPool.BUFFER_SIZE > Mergesort.FILE_SIZE)
+                {
+                    file.read(block, 0, Mergesort.FILE_SIZE
+                            - (index * BufferPool.BUFFER_SIZE));
+                }
+                else
+                    file.read(block);
                 RuntimeStats.readDisk++;
             }
         }
