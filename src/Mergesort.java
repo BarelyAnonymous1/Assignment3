74404b8c4904db8b8a8c908944038e8bb968b35c
Jonathan <jondef95@vt.edu>
2016-04-07 14:54:03 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/MergesortTest.java changed at Thu Apr 07 14:54:02 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -54,7 +54,7 @@ public class Mergesort
         {
             input = new RandomAccessFile("input1.txt", "rw");
             temp = new RandomAccessFile("test.txt", "rw");
-            BufferPool bufpool = new BufferPool(5);
+            BufferPool bufpool = new BufferPool(15);
             // byte [] output = bufpool.getRecord(0, input);
             // System.out.println(output[1]);
             // bufpool.tempRecord(0, input);
