9a7c96d25bfa970719c44f03379c754401b155a6
jondef95@vt.edu <jondef95@github.com>
2016-04-07 16:46:26 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/RuntimeStats.java changed at Thu Apr 07 16:46:26 EDT 2016

--- src/RuntimeStats.java
+++ src/RuntimeStats.java
@@ -1,31 +1,22 @@
 
 public class RuntimeStats
 {
-    private static String file;
-    private static int    numBuffers;
-    public static int     foundInBuffer;
-    public static int     readDisk;
-    public static int     writeDisk;
-    public static long    startTime;
-    public static long    endTime;
-    
-    public RuntimeStats(String filename, int numberOfBuffers)
-    {
-        file = filename;
-        numBuffers = numberOfBuffers;
-        foundInBuffer = 0;
-        readDisk = 0;
-        writeDisk = 0;
-        startTime = 0;
-        endTime = 0;
-    }
+    public static String file = "";
+    public static int    numBuffers = 0;
+    public static int    foundInBuffer = 0;
+    public static int    readDisk = 0;
+    public static int    writeDisk = 0;
+    public static long   startTime = 0;
+    public static long   endTime = 0;
+
+  
 
     public static String toStaticString()
     {
         long total = endTime - startTime;
         return ("Filename: " + file + "\nNumber of Buffers: " + numBuffers
-                + "\nCache Hits: " + foundInBuffer
-                + "\nReads: " + readDisk + "\nWrites: "
-                + writeDisk + "\nTotal Time: " + total + " milliseconds\n");
+                + "\nCache Hits: " + foundInBuffer + "\nReads: " + readDisk
+                + "\nWrites: " + writeDisk + "\nTotal Time: " + total
+                + " milliseconds\n");
     }
 }
