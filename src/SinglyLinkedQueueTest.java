fc68a10695fa6fe06c4a8eb19b1bcd14870285b1
Preston Lattimer <preston.lattimer@gmail.com>
2016-04-07 13:40:01 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment3/src/SinglyLinkedQueueTest.java changed at Thu Apr 07 13:40:01 EDT 2016

--- src/SinglyLinkedQueueTest.java
+++ src/SinglyLinkedQueueTest.java
@@ -13,10 +13,11 @@ public class SinglyLinkedQueueTest extends TestCase {
 
     public void testInsert()
     {
-        Buffer buffer = new Buffer(5, null);
+        Buffer buffer = new Buffer(3, null);
         Buffer buffer2 = new Buffer(10, null);
         list.enqueue(new SinglyLinkedNode(buffer));
         list.enqueue(new SinglyLinkedNode(buffer2));
+        list.enqueue(new SinglyLinkedNode(buffer));
         System.out.println(list.toString());
     }
     
