f5e997fb4123749b87f0981f99076c66bb0344d4
jondef95@vt.edu <jondef95@github.com>
2016-04-07 18:09:07 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Mergesort.java changed at Thu Apr 07 18:08:57 EDT 2016

--- src/Mergesort.java
+++ src/Mergesort.java
@@ -30,6 +30,7 @@ public class Mergesort
                 return;
             }
             input = new RandomAccessFile(args[0], "rw");
+            RuntimeStats.file = args[0];
             temp = new RandomAccessFile("temp", "rw");
             temp.setLength(0); // empties the temp files used
             BufferPool pool = new BufferPool(Integer.parseInt(args[1]));
