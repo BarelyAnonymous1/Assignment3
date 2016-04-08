95f40710e84bda60e74031f5a49ecd0eb48939eb
jondef95@vt.edu <jondef95@github.com>
2016-04-07 23:27:20 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/MergesortTest.java changed at Thu Apr 07 23:27:20 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -40,7 +40,7 @@ public class MergesortTest
 	    args[2] = "statFile.txt";
         genargs[0] = "-a";
 	    genargs[1] = "test.txt";
-	    genargs[2] = "4";
+	    genargs[2] = "1000";
 	    //String numBlocks = "100"; // Test file size
 	    generator.generateFile(genargs);
 	    Mergesort.main(args);
