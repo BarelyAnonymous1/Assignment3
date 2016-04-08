58c0694c8132e8cd106b51666b564dc0111768b4
jondef95@vt.edu <jondef95@github.com>
2016-04-08 00:07:53 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/MergesortTest.java changed at Fri Apr 08 00:07:52 EDT 2016

--- src/MergesortTest.java
+++ src/MergesortTest.java
@@ -7,43 +7,45 @@ import student.TestCase;
  * @version 1
  */
 
-public class MergesortTest 
-	extends TestCase {
-	
-	/**
-	 * This method sets up the tests that follow.
-	 */
-	public void setUp() {
-		// no op
-	}
-	
-	/**public void testInit() {
-		Mergesort merge = new Mergesort();
-		assertNotNull(merge);
-		Mergesort.main(null);
-		assertFuzzyEquals("Hello, World", systemOut().getHistory());
-	}*/
-	/**
-	 * This method tests the main functionality of Mergesort on an "ascii" file
-	 *
-	 * @throws Exception
-	 * either a IOException or FileNotFoundException
-	 */
-	public void testMergesortAscii4()
-	    throws Exception
-	{
-	    FileGenerator generator = new FileGenerator();
-	    String[] args = new String[3];
-	    String[] genargs = new String[3];
-	    args[0] = "input12a.txt";
-	    args[1] = "15"; // Buffer pool size
-	    args[2] = "statFile.txt";
-        genargs[0] = "-a";
-	    genargs[1] = "input12a.txt";
-	    genargs[2] = "1000";
-	    //String numBlocks = "100"; // Test file size
-	    generator.generateFile(genargs);
-	    Mergesort.main(args);
+public class MergesortTest extends TestCase
+{
 
-	}
+    /**
+     * This method sets up the tests that follow.
+     */
+    public void setUp()
+    {
+        // no op
+    }
+
+    public void testInit()
+    {
+        Mergesort merge = new Mergesort();
+        assertNotNull(merge);
+        Mergesort.main(null);
+        assertFuzzyEquals("Hello, World", systemOut().getHistory());
+    }
+
+    /**
+     * This method tests the main functionality of Mergesort on an "ascii" file
+     *
+     * @throws Exception
+     *             either a IOException or FileNotFoundException
+     */
+    public void testMergesortAscii4() throws Exception
+    {
+        FileGenerator generator = new FileGenerator();
+        String[] args = new String[3];
+        String[] genargs = new String[3];
+        args[0] = "input12a.txt";
+        args[1] = "15"; // Buffer pool size
+        args[2] = "statFile.txt";
+        genargs[0] = "-a";
+        genargs[1] = "input12a.txt";
+        genargs[2] = "1000";
+        // String numBlocks = "100"; // Test file size
+        generator.generateFile(genargs);
+        Mergesort.main(args);
+
+    }
 }
