41d25f8255b977470df71882bbe8406094fc5b2f
jondef95@vt.edu <jondef95@github.com>
2016-04-07 22:21:17 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Buffer.java changed at Thu Apr 07 22:21:17 EDT 2016

--- src/Buffer.java
+++ src/Buffer.java
@@ -1,36 +1,71 @@
 import java.io.*;
 
+/**
+ * Buffer class to implement a buffer pool. Contains a block for the byte array,
+ * which has a max size determined by a constant in the buffer pool class. The
+ * buffer reads from a file to find its block, the edits that block before
+ * writing back to the file.
+ * 
+ * @author Jonathan DeFreeuw (jondef95) Preston Lattimer (platt)
+ *
+ */
 public class Buffer
 {
+    /**
+     * block to hold a set amount of bytes from the file
+     */
     private byte[]           block;
+    /**
+     * determines where in the file the block came from 0 means the first 0-4095
+     * bytes, 1 is 4096-8191, etc
+     */
     private int              index;
+    /**
+     * determines if the block has been edited (new record has been placed)
+     */
     private boolean          dirtyBit;
-    private int              size;
+    /**
+     * the index of the furthest record that has been edited in the block; helps
+     * the buffer not write the entire block if it can help it; optimization
+     * decision
+     */
     private int              furthestByte;
-
+    /**
+     * the specific file that the block has been read from and will write to
+     */
     private RandomAccessFile file;
 
     /**
      * constructor for the Buffer class This class will do the file I/O to
      * interface with the BufferPool
      * 
-     * @param pageIndex
+     * @param startID
+     *            the initial index for the buffer, determines where in the file
+     *            the first block will come from
      * @param startFile
+     *            the initial file for the buffer, determines which file the
+     *            first block will come from
      */
     public Buffer(int startID, RandomAccessFile startFile)
     {
-        block = new byte[BufferPool.BUFFER_SIZE];
+        block = new byte[BufferPool.BUFFER_SIZE]; // create the array necessary
+                                                  // for operation
         index = startID;
         file = startFile;
         dirtyBit = false;
         furthestByte = 0;
-        storeBlock();
+        storeBlock(); // get the initial block to store
     }
 
-    public void reset(int startPosition, RandomAccessFile startFile)
+    /**
+     * 
+     * @param resetID
+     * @param resetFile
+     */
+    public void reset(int resetID, RandomAccessFile resetFile)
     {
-        index = startPosition / BufferPool.BUFFER_SIZE;
-        file = startFile;
+        index = resetID / BufferPool.BUFFER_SIZE;
+        file = resetFile;
         dirtyBit = false;
         furthestByte = 0;
         storeBlock();
@@ -45,21 +80,17 @@ public class Buffer
     {
         try
         {
-                file.seek(index * BufferPool.BUFFER_SIZE);
-                if ((index + 1)
-                        * BufferPool.BUFFER_SIZE > Mergesort.FILE_SIZE)
-                {
-                    file.read(block, 0, Mergesort.FILE_SIZE
-                            - (index * BufferPool.BUFFER_SIZE));
-                    size = (Mergesort.FILE_SIZE
-                            - (index * BufferPool.BUFFER_SIZE));
-                }
-                else
-                {
-                    file.read(block);
-                    size = BufferPool.BUFFER_SIZE;
-                }
-                RuntimeStats.readDisk++;
+            file.seek(index * BufferPool.BUFFER_SIZE);
+            if ((index + 1) * BufferPool.BUFFER_SIZE > Mergesort.FILE_SIZE)
+            {
+                file.read(block, 0, Mergesort.FILE_SIZE
+                        - (index * BufferPool.BUFFER_SIZE));
+            }
+            else
+            {
+                file.read(block);
+            }
+            RuntimeStats.readDisk++;
         }
         catch (IOException e)
         {
@@ -94,7 +125,7 @@ public class Buffer
         block[recordNum + 1] = newPage[1];
         block[recordNum + 2] = newPage[2];
         block[recordNum + 3] = newPage[3];
-        
+
         if (furthestByte < recordNum + 4)
             furthestByte = recordNum + 4;
         // System.arraycopy(record, 0, block, recordNum,
