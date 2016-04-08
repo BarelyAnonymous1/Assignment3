ffbca2e553c16189669e0b424cbf6581f7316f16
jondef95@vt.edu <jondef95@github.com>
2016-04-07 22:01:20 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/RuntimeStats.java changed at Thu Apr 07 22:01:20 EDT 2016

--- src/RuntimeStats.java
+++ src/RuntimeStats.java
@@ -1,15 +1,22 @@
-
+/**
+ * Class that is used to keep count of a set of stats on the BufferPool-based
+ * Mergesort. Is abstract to there is no way to insantiate the class. 
+ * 
+ * @author Jonathan
+ *
+ */
 public abstract class RuntimeStats
 {
-    public static String file = "";
-    public static int    numBuffers = 0;
+    /**
+     * 
+     */
+    public static String file          = "";
+    public static int    numBuffers    = 0;
     public static int    foundInBuffer = 0;
-    public static int    readDisk = 0;
-    public static int    writeDisk = 0;
-    public static long   startTime = 0;
-    public static long   endTime = 0;
-
-  
+    public static int    readDisk      = 0;
+    public static int    writeDisk     = 0;
+    public static long   startTime     = 0;
+    public static long   endTime       = 0;
 
     public static String toStaticString()
     {
