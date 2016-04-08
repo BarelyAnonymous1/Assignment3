12172a770504970d2c0537013244baa42a533b59
Preston Lattimer <preston.lattimer@gmail.com>
2016-04-07 23:21:04 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment3/src/DoublyLinkedNode.java changed at Thu Apr 07 23:21:04 EDT 2016

--- src/DoublyLinkedNodeTest.java
+++ src/DoublyLinkedNodeTest.java
@@ -1,12 +1,26 @@
-import org.junit.Test;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
 
 import student.TestCase;
 
 public class DoublyLinkedNodeTest extends TestCase {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    private DoublyLinkedNode node1;
+    private DoublyLinkedNode node2;
+    public void setUp() throws FileNotFoundException
+    {
+        RandomAccessFile file = new RandomAccessFile("buffertest.txt", "rw");
+        node1 = new DoublyLinkedNode(new Buffer(2, file));
+        node1 = new DoublyLinkedNode(new Buffer(2, file));
     }
 
+    /**
+     * tests the node class
+     */
+    public void testNodes()
+    {
+        node1.setNext(node2);
+        assertEquals(node1, node2.prev);
+    }
+    
 }
