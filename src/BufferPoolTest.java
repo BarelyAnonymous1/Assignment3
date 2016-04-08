f09d81eebb7bd612dc968b41db0c909dfd3e7a06
Preston Lattimer <preston.lattimer@gmail.com>
2016-04-07 22:57:31 -0400

    File /Users/prestonlattimer/Documents/workspace/Assignment3/src/BufferPoolTest.java changed at Thu Apr 07 22:57:18 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -1,3 +1,5 @@
+import java.io.RandomAccessFile;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,14 +7,22 @@ import student.TestCase;
 
 public class BufferPoolTest extends TestCase {
 
-    @Before
-    protected void setUp() throws Exception {
-        super.setUp();
+    BufferPool buffpool;
+    RandomAccessFile file;
+    /**
+     * sets up the tests
+     */
+    public void setUp()
+    {
+        buffpool = new BufferPool(3);
+        file = new RandomAccessFile("test.txt", "rw");
     }
-
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    
+    /**
+     * tests the allocation of the buffer
+     */
+    public void testAllocateBuffer()
+    {
+        
     }
-
 }
