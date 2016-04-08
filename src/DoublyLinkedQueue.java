17e692d00d1bce60fd1682657f48381c0b18347e
jondef95@vt.edu <jondef95@github.com>
2016-04-08 00:19:14 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/DoublyLinkedQueue.java changed at Fri Apr 08 00:19:14 EDT 2016

--- src/DoublyLinkedQueue.java
+++ src/DoublyLinkedQueue.java
@@ -91,7 +91,7 @@ public class DoublyLinkedQueue
         while (curr != head)
         {
             Buffer buffer = curr.getData();
-            if (buffer.getID() == blockID && buffer.getFile() != null
+            if (buffer.getID() == blockID //&& buffer.getFile() != null
                     && buffer.getFile() == file)
             {
                 curr.prev.setNext(curr.next);
