2a0e0d9daffb1c39d8be2d8c51a1bf5f43c76447
jondef95@vt.edu <jondef95@github.com>
2016-04-07 23:24:30 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferTest.java changed at Thu Apr 07 23:24:30 EDT 2016

--- src/BufferTest.java
+++ src/BufferTest.java
@@ -6,17 +6,22 @@ import org.junit.Test;
 import student.TestCase;
 
 /**
- * @author prestonlattimer
+ * Tests the methods for the Buffer to make sure it properly reads and writes
+ * from a file
+ * 
+ * @author Preston Lattimer (platt) Jonathan DeFreeuw (jondef95)
  *
  */
-public class BufferTest extends TestCase {
+public class BufferTest extends TestCase
+{
 
     RandomAccessFile file;
-    Buffer buffer; 
-    
-    protected void setUp() {
-        //file = new RandomAccessFile("input.txt");
-        //buffer = new Buffer(0, );
+    Buffer           buffer;
+
+    protected void setUp()
+    {
+        // file = new RandomAccessFile("input.txt");
+        // buffer = new Buffer(0, );
     }
 
 }
