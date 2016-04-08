2c27544b042902355b695ab82b4ca55416e79aae
jondef95@vt.edu <jondef95@github.com>
2016-04-07 22:48:31 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/Buffer.java changed at Thu Apr 07 22:48:31 EDT 2016

--- src/Buffer.java
+++ src/Buffer.java
@@ -125,7 +125,7 @@ public class Buffer
 
     public void getRecord(byte[] record, int pos)
     {
-        record = Arrays.copyOfRange(block, pos, BufferPool.RECORD_SIZE + 1);
+        record = Arrays.copyOfRange(block, pos, pos + BufferPool.RECORD_SIZE);
     }
 
     public void setBlock(byte[] newPage, int recordNum)
