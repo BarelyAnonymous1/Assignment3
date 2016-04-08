3102e5afcb70e57123900d18b569e4edca256b47
jondef95@vt.edu <jondef95@github.com>
2016-04-07 23:23:55 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferPoolTest.java changed at Thu Apr 07 23:23:55 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -8,30 +8,35 @@ import org.junit.Test;
 import student.TestCase;
 
 /**
+ * Tests the methods in the BufferPool class to make sure that the methods work
+ * appropriately
  * 
  * @author Preston Lattimer (platt) Jonathan DeFreeuw (jondef95)
  *
  */
-public class BufferPoolTest extends TestCase {
+public class BufferPoolTest extends TestCase
+{
 
-    BufferPool buffpool;
+    BufferPool       buffpool;
     RandomAccessFile file;
+
     /**
      * sets up the tests
-     * @throws FileNotFoundException 
+     * 
+     * @throws FileNotFoundException
      */
     public void setUp() throws FileNotFoundException
     {
         buffpool = new BufferPool(3);
         file = new RandomAccessFile("buffertest.txt", "rw");
     }
-    
+
     /**
      * tests the allocation of the buffer
      */
     public void testAllocateBuffer()
     {
         assertEquals(buffpool, buffpool.allocateBuffer(0, file));
-        
+
     }
 }
