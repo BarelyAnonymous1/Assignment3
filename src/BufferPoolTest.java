4ce113e8ecf641679be631ab212b6a0037413391
jondef95@vt.edu <jondef95@github.com>
2016-04-08 17:56:06 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferPoolTest.java changed at Fri Apr 08 17:56:06 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -33,7 +33,7 @@ public class BufferPoolTest extends TestCase
             test[i] = "a".getBytes()[0];
         for (int j = 0; j < 4096; j++)
             test2[j] = "b".getBytes()[0];
-        for (int j = 0; j < 4096; j++)
+        for (int k = 0; j < 4096; j++)
             test2[k] = "c".getBytes()[0];
         file.write(test);
         file.write(test2);
@@ -74,10 +74,14 @@ public class BufferPoolTest extends TestCase
         assertTrue(buffpool.getSize() == 2);
     }
 
-    public void testWriteRecord()
+    public void testWriteRecord() throws IOException
     {
         buffpool.allocateBuffer(0, file);
         buffpool.allocateBuffer(4096, file);
-        buffpool.allocateBuffer(8192, file);
+        byte[] sample = new byte[4];
+        byte[] compare = "cccc".getBytes();
+        buffpool.getRecord(8192, sample, file);
+        assertTrue(ByteBuffer.wrap(sample).compareTo(ByteBuffer.wrap(
+                compare)) == 0);
     }
 }
