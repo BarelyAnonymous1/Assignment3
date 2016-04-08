b571904ab39be858c2f08d635777555981e7a7cc
jondef95@vt.edu <jondef95@github.com>
2016-04-07 20:32:19 -0400

    File /C:/Users/Jonathan/git/Assignment3/src/BufferPool.java changed at Thu Apr 07 20:32:18 EDT 2016

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -7,7 +7,6 @@ public class BufferPool
     public static int    RECORD_SIZE = 4;
     private LRUQueue     pool;
     private int          maxBuffers;
-    public static byte[] TEMP_REC;
 
     /**
      * initializes the bufferpool with -1, -2, -3, ... for whatever the startMax
@@ -21,7 +20,6 @@ public class BufferPool
     {
         maxBuffers = startMax;
         pool = new LRUQueue(startMax);
-        TEMP_REC = new byte[RECORD_SIZE];
         for (int i = 0; i < maxBuffers; i++)
         {
             // the ID for each filler Buffer is so the Buffer pool knows to do
